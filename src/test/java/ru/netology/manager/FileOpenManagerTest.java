c080ad1736b120f419ebdfd1ae87d64582a95a73
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-05 16:02:25 +0300

    Задача 2

--- src/test/java/ru/netology/manager/FileOpenManagerTest.java
+++ src/test/java/ru/netology/manager/FileOpenManagerTest.java
@@ -11,32 +11,31 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class FileOpenManagerTest {
     private FileOpenManager fileOpenManager = new FileOpenManager();
 
-
     private String office = "Word";
     private String browser = "Google Chrome";
     private String image = "Adobe Photoshop";
-
 
     private String doc = ".doc";
     private String html = ".html";
     private String png = ".png";
     private String jpg = ".jpg";
+    private String bmp = ".bmp";
 
 
     @BeforeEach
     void setUp() {
-        fileOpenManager.register(doc,office );
+        fileOpenManager.register(doc, office);
         fileOpenManager.register(html, browser);
-        fileOpenManager.remove(".jpg");
         fileOpenManager.register(png, image);
         fileOpenManager.register(jpg, image);
+        fileOpenManager.register(bmp, image);
     }
 
     @Test
     void shouldGetAllApps() {
         Set<String> actual = fileOpenManager.getAllApps();
         Set<String> expected = new HashSet<>();
-          expected.add(image);
+        expected.add(image);
         expected.add(browser);
         expected.add(office);
         assertEquals(expected, actual);
@@ -46,7 +45,7 @@ class FileOpenManagerTest {
     @Test
     void shouldGetAllExtension() {
         Collection<String> actual = fileOpenManager.getAllExtension();
-        List<String> expected = Arrays.asList(doc, html, jpg, png);
+        List<String> expected = Arrays.asList(bmp, doc, html, jpg, png);
         assertEquals(expected, actual);
     }
 
@@ -60,7 +59,7 @@ class FileOpenManagerTest {
 
 
     @Test
-    void shouldShowNameByTypeUsedUppercase() {
+    void shouldGetNameByTypeUsedUppercase() {
         String expected = "Adobe Photoshop";
         String actual = fileOpenManager.getName(".JPG");
         assertEquals(expected, actual);
